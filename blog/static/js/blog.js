8386ccb39390fa3716327592a518d6b6df72d25e
naman-32 <nams.goenka@gmail.com>
2019-12-23 09:58:47 +0530

    initial commit

---
+++ blog/static/js/blog.js
@@ -0,0 +1,18 @@
+// var like = document.getElementById('to_be_liked');
+// var x = like.parentElement;
+// console.log(like);
+// console.log(x);
+// // document.getElementById('to_be_liked').addEventListener("click",
+// like.onclick = function(){
+// console.log("dsa");
+//   if ( like.classList.contains('fa fa-heart-o') ){
+//     console.log("dsa");
+//      like.className = 'fa fa-heart';
+//
+//      // x.setAttribute("href", "{% url 'like_remove' pk=post.pk %}");
+//   }
+//   else{
+//       like.className = 'fa fa-heart-o';
+//       // x.setAttribute("href", "{% url 'like_add' pk=post.pk %}");
+//   }
+// };
